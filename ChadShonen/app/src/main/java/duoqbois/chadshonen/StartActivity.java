00d3ce57310b692c091643912de4625bae104268
Patrik Durasek <pxd7081@rit.edu>
2017-10-23 12:01:08 +0200

    Finish Register and Login (#1)
    - finish register feature
    - finish logn feature
    
    TODO
    - add comments
    - handle exception

--- ChadShonen/app/src/main/java/duoqbois/chadshonen/StartActivity.java
+++ ChadShonen/app/src/main/java/duoqbois/chadshonen/StartActivity.java
@@ -10,6 +10,7 @@ public class StartActivity extends AppCompatActivity
 {
 
     private Button mRegBtn;
+    private Button mLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,6 +19,7 @@ public class StartActivity extends AppCompatActivity
         setContentView(R.layout.activity_start);
 
         mRegBtn = (Button) findViewById(R.id.startRegButton);
+        mLoginBtn = (Button) findViewById(R.id.start_login_button);
 
         mRegBtn.setOnClickListener(new View.OnClickListener()
         {
@@ -26,6 +28,16 @@ public class StartActivity extends AppCompatActivity
             {
                 Intent reg_intent = new Intent(StartActivity.this, RegisterActivity.class);
                 startActivity(reg_intent);
+            }
+        });
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent loginIntent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
             }
         });
     }
