98aa9e483e3613e4e0edc5fe1e22654a76c7e219
Patrik Durasek <pxd7081@rit.edu>
2017-10-27 21:40:23 +0200

    Store User data to Realtime Database
    - store user data on registration
    - create profile page

--- ChadShonen/app/src/main/java/duoqbois/chadshonen/MainActivity.java
+++ ChadShonen/app/src/main/java/duoqbois/chadshonen/MainActivity.java
@@ -1,6 +1,7 @@
 package duoqbois.chadshonen;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
